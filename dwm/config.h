fb3ee09f022a62cc3b91bb9f362214c2f9a0acc7
George-Markas <georgemarkanton@proton.me>
2025-04-27 03:05:16 +0300

    Changed dwm colors

--- dwm/config.h
+++ dwm/config.h
@@ -1,27 +1,32 @@
 /* See LICENSE file for copyright and license details. */
 
 /* appearance */
-static const unsigned int borderpx  = 1;        /* border pixel of windows */
-static const Gap default_gap        = {.isgap = 1, .realgap = 10, .gappx = 6};
-static const unsigned int snap      = 32;       /* snap pixel */
-static const unsigned int systraypinning = 0;   /* 0: sloppy systray follows selected monitor, >0: pin systray to monitor X */
-static const unsigned int systrayonleft  = 0;   /* 0: systray in the right corner, >0: systray on left of status text */
-static const unsigned int systrayspacing = 2;   /* systray spacing */
-static const int systraypinningfailfirst = 1;   /* 1: if pinning fails, display systray on the first monitor, False: display systray on the last monitor*/
-static const int showsystray        = 1;        /* 0 means no systray */
-static const int showbar            = 1;        /* 0 means no bar */
-static const int topbar             = 1;        /* 0 means bottom bar */
-static const char *fonts[]          = { "Hack Nerd Font:size=14" };
-static const char dmenufont[]       = "monospace:size=13";
-static const char col_gray1[]       = "#222222";
-static const char col_gray2[]       = "#444444";
-static const char col_gray3[]       = "#bbbbbb";
-static const char col_gray4[]       = "#eeeeee";
-static const char col_cyan[]        = "#005577";
-static const char *colors[][3]      = {
-	/*               fg         bg         border   */
-	[SchemeNorm] = { col_gray3, col_gray1, col_gray2 },
-	[SchemeSel]  = { col_gray4, col_cyan,  col_cyan  },
+static const unsigned int borderpx          = 1;        /* border pixel of windows */
+static const unsigned int snap              = 32;       /* snap pixel */
+static const unsigned int systraypinning    = 0;        /* 0: sloppy systray follows selected monitor, >0: pin systray to monitor X */
+static const unsigned int systrayonleft     = 0;        /* 0: systray in the right corner, >0: systray on left of status text */
+static const unsigned int systrayspacing    = 2;        /* systray spacing */
+static const int systraypinningfailfirst    = 1;        /* 1: if pinning fails, display systray on the first monitor, False: display systray on the last monitor*/
+static const int showsystray                = 1;        /* 0 means no systray */
+static const int showbar                    = 1;        /* 0 means no bar */
+static const int topbar                     = 1;        /* 0 means bottom bar */
+static const Gap default_gap                = {.isgap = 1, .realgap = 10, .gappx = 6};
+static const char *fonts[]                  = { "Hack Nerd Font:size=14" };
+static const char dmenufont[]               = "monospace:size=13";
+static const char col_gray1[]               = "#222222";
+static const char col_gray2[]               = "#444444";
+static const char col_gray3[]               = "#bbbbbb";
+static const char col_gray4[]               = "#eeeeee";
+static const char col_cyan[]                = "#005577";
+static const char light_gray[]              = "#999999";
+static const char gray[]                    = "#303030";
+static const char dark_gray[]               = "#101010";
+static const char blue[]                    = "#224488";
+static const char white[]                   = "#ffffff";
+static const char *colors[][3]              = {
+    /*               fg         bg         border   */
+	[SchemeNorm] = { light_gray, dark_gray, gray },
+	[SchemeSel]  = { white, blue, blue },
 };
 
 /* tagging */
@@ -42,7 +47,7 @@ static const float mfact        = 0.55; /* factor of master area size [0.05..0.9
 static const int nmaster        = 1;    /* number of clients in master area */
 static const int resizehints    = 1;    /* 1 means respect size hints in tiled resizals */
 static const int attachbelow    = 1;    /* 1 means attach after the currently active window */
-static const int lockfullscreen = 1; /* 1 will force focus on the fullscreen window */
+static const int lockfullscreen = 1;    /* 1 will force focus on the fullscreen window */
 
 static const Layout layouts[] = {
 	/* symbol     arrange function */
