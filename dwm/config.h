47f6f96d5b75e4044828bcb2f61f6c10033d8662
George-Markas <georgemarkanton@proton.me>
2025-04-27 21:24:16 +0300

    Added swallow patch to dwm, upated lf opener to work with it

--- dwm/config.h
+++ dwm/config.h
@@ -8,6 +8,7 @@ static const unsigned int systrayonleft     = 0;        /* 0: systray in the rig
 static const unsigned int systrayspacing    = 2;        /* systray spacing */
 static const int systraypinningfailfirst    = 1;        /* 1: if pinning fails, display systray on the first monitor, False: display systray on the last monitor*/
 static const int showsystray                = 1;        /* 0 means no systray */
+static const int swallowfloating            = 0;        /* 1 means swallow floating windows by default */
 static const int showbar                    = 1;        /* 0 means no bar */
 static const int topbar                     = 1;        /* 0 means bottom bar */
 static const Gap default_gap                = {.isgap = 1, .realgap = 10, .gappx = 6};
@@ -29,9 +30,9 @@ static const Rule rules[] = {
 	 *	WM_CLASS(STRING) = instance, class
 	 *	WM_NAME(STRING) = title
 	 */
-	/* class      instance    title       tags mask     isfloating   monitor */
-	{ "Gimp",     NULL,       NULL,       0,            1,           -1 },
-	{ "Firefox",  NULL,       NULL,       1 << 8,       0,           -1 },
+	/* class     instance  title           tags mask  isfloating  isterminal  noswallow  monitor */
+	{ "kitty",   NULL,     NULL,           0,         0,          1,           0,        -1 },
+	{ NULL,      NULL,     "Event Tester", 0,         0,          0,           1,        -1 }, /* xev */
 };
 
 /* layout(s) */
