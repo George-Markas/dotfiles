b398a30d9e179d05e9e505e095e1d525088ac909
George-Markas <georgemarkanton@proton.me>
2025-05-16 19:43:46 +0300

    Changed font

--- dwm/config.def.h
+++ dwm/config.def.h
@@ -12,7 +12,7 @@ static const int showsystray        = 1;        /* 0 means no systray */
 static const int swallowfloating    = 0;        /* 1 means swallow floating windows by default */
 static const int showbar            = 1;        /* 0 means no bar */
 static const int topbar             = 1;        /* 0 means bottom bar */
-static const char *fonts[]          = { "Hack Nerd Font:size=15" };
+static const char *fonts[]          = { "Iosevka Nerd Font:size=15:antialias=true:autohint=true" };
 static const char dmenufont[]       = "monospace:size=10";
 
 #include "colorscheme.h"
