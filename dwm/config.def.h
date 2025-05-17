5ee4357d6d1902b6a42f6274c3ec5815d16aa950
George-Markas <georgemarkanton@proton.me>
2025-05-17 03:41:56 +0300

    Added an emoji font for proper rendering in the bar

--- dwm/config.def.h
+++ dwm/config.def.h
@@ -11,7 +11,7 @@ static const unsigned int systraypinning = 0;   /* 0: sloppy systray follows sel
 static const unsigned int systrayspacing = 2;   /* systray spacing */
 static const int systraypinningfailfirst = 1;   /* 1: if pinning fails, display systray on the first monitor, False: display systray on the last monitor*/
 static const int showsystray             = 1;   /* 0 means no systray */
-static const char *fonts[]          = { "Iosevka Nerd Font:size=15:antialias=true:autohint=true" };
+static const char *fonts[]          = { "Iosevka Nerd Font:size=15:antialias=true:autohint=true", "NotoColorEmoji:pixelsize=15:antialias=true:autohint=true" };
 static const char dmenufont[]       = "monospace:size=10";
 static const char col_gray1[]       = "#222222";
 static const char col_gray2[]       = "#444444";
