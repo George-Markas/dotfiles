e50ea66e9aa619db081a937a4b14d12157443a5e
George-Markas <georgemarkanton@proton.me>
2025-05-28 16:48:41 +0300

    Replaced reflow with column redraw

--- st/config.mk
+++ st/config.mk
@@ -15,11 +15,11 @@ PKG_CONFIG = pkg-config
 # includes and libs
 INCS = -I$(X11INC) \
        `$(PKG_CONFIG) --cflags fontconfig` \
-       `$(PKG_CONFIG) --cflags freetype2` \
+	   `$(PKG_CONFIG) --cflags freetype2` \
 	   `$(PKG_CONFIG) --cflags harfbuzz`
 LIBS = -L$(X11LIB) -lm -lrt -lX11 -lutil -lXft \
        `$(PKG_CONFIG) --libs fontconfig` \
-       `$(PKG_CONFIG) --libs freetype2` \
+	   `$(PKG_CONFIG) --libs freetype2` \
 	   `$(PKG_CONFIG) --libs harfbuzz`
 
 # flags
