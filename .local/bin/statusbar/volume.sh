817f52f9644e79b18e50727e05b127e7f902736d
George-Markas <georgemarkanton@proton.me>
2025-06-02 04:43:38 +0300

    Readded click actions to volume script

--- .local/bin/statusbar/volume.sh
+++ .local/bin/statusbar/volume.sh
@@ -1,5 +1,12 @@
 #!/bin/sh
 
+case "$BLOCK_BUTTON" in
+    1) pavucontrol ;;
+    3) wpctl set-mute @DEFAULT_AUDIO_SINK@ toggle; kill -42 $(pidof dwmblocks) ;;
+    4) wpctl set-volume -l 1.0 @DEFAULT_AUDIO_SINK@ 5%+; kill -42 $(pidof dwmblocks) ;;
+    5) wpctl set-volume @DEFAULT_AUDIO_SINK@ 5%-; kill -42 $(pidof dwmblocks) ;;
+esac
+
 vol_info=$(wpctl get-volume @DEFAULT_AUDIO_SINK@)
 muted=$(echo "$vol_info" | grep -o "MUTED")
 
