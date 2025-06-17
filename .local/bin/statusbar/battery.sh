cdaebdf1aeb7901ebbd459ffe52488f4264eb204
George-Markas <georgemarkanton@proton.me>
2025-06-17 15:41:18 +0300

    Added battery status script

---
+++ .local/bin/statusbar/battery.sh
@@ -0,0 +1,19 @@
+#!/bin/sh
+
+battery="/sys/class/power_supply/BAT0"
+adapter="/sys/class/power_supply/ADP0"
+
+bat_perc="$(cat "$battery/capacity" 2>&1)%"
+ac_conn=$(cat "$adapter/online" 2>&1)
+
+if [ $ac_conn -eq 1 ]; then
+    if [ "$battery/status" = "Charging" ]; then
+        label="CHRG"
+    else
+        label="AC"
+    fi
+else
+    label="BAT"
+fi
+
+echo "$label: $bat_perc"
