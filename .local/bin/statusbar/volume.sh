6afc036b7ea1c42424cd3e8d98873d3587ce9435
George-Markas <georgemarkanton@proton.me>
2025-06-18 19:09:17 +0300

    Updated volume script

--- .local/bin/statusbar/volume.sh
+++ .local/bin/statusbar/volume.sh
@@ -1,12 +1,12 @@
 #!/bin/sh
 
-vol_info=$(wpctl get-volume @DEFAULT_AUDIO_SINK@)
-muted=$(echo "$vol_info" | grep -o "MUTED")
+vol=$(wpctl get-volume @DEFAULT_AUDIO_SINK@)
+muted=$(awk '{print $3}' <<< "$vol")
 
 if [ -n "$muted" ]; then
     vol_perc="Muted"
 else
-    vol_perc="$(echo "$vol_info" | awk '{printf "%.0f", substr($0,9,4) * 100}' )%"
+    vol_perc="$(awk '{ printf "%d", $2 * 100 }' <<< "$vol")%"
 fi
 
 echo "VOL: $vol_perc"
